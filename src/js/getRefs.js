bda305b34ad4d90aca8a8be79f8861685da4d864
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-01 15:31:43 +0300

    Create getRefs.js

---
+++ src/js/getRefs.js
@@ -0,0 +1,9 @@
+/* export default */ function getRefs() {
+    return {
+        searchForm: document.querySelector('.js-search'),
+        containerList: document.querySelector('.container-cards'),
+    };
+    
+}
+
+export default getRefs;
